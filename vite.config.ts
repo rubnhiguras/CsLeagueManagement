555290b364134d880125fec89a4171f1d3acbb60
rubnhiguras <proyectoshigueras@gmail.com>
2025-10-21 23:21:46 +0200

    version 0.3.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({ 
-  plugins: [react()],
+  plugins: [react(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'CSLeague App',
+        short_name: 'CSLeague',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icons/CSLeague_icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/CSLeague_icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
